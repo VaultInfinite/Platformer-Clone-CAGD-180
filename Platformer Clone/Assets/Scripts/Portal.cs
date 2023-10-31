ef57709a76db6e1aaeadb22e397c663e84308489
k1kalkat <karenkalkat0915@gmail.com>
2023-10-31 09:48:25 -0700

    Finished Level Two and Created Base for Hard Enemy

---
+++ Platformer Clone/Assets/Scripts/Portal.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Kalkat, Karen]
+ * Last Updated: [10/31/2023]
+ * [Contains the code for the Portal]
+ */
+
+public class Portal : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
